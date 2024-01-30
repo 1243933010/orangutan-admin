0ebf3a7a89d3d31e64edc0bac26c8055f96c700c
ysys <1243933010@qq.com>
2024-01-30 10:19:20 +0800

    add project

---
+++ src/utils/auth.js
@@ -0,0 +1,17 @@
+import Cookies from 'js-cookie'
+
+const TokenKey = 'Admin-Token'
+
+export function getToken() {
+  return Cookies.get(TokenKey)
+}
+
+export function setToken(token) {
+  Cookies.set('token', token)
+  return Cookies.set(TokenKey, token)
+}
+
+export function removeToken() {
+  Cookies.remove('token')
+  return Cookies.remove(TokenKey)
+}
